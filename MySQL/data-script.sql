7202366c61b2e6b823aad513bad6e03c314d71b4
Adriano M. Martins <adrianomqsmts@gmail.com>
2021-11-04 22:48:46 -0300

    .gitignore

---
+++ MySQL/data-script.sql
@@ -0,0 +1,93 @@
+-- Inserindo Usuários de teste
+
+INSERT INTO `onefigure`.`usuario` (`name`, `password`) VALUES ('teste', 'teste');
+INSERT INTO `onefigure`.`usuario` (`name`, `password`) VALUES ('user', 'user');
+INSERT INTO `onefigure`.`usuario` (`name`, `password`) VALUES ('fulano', 'fulano');
+INSERT INTO `onefigure`.`usuario` (`name`, `password`) VALUES ('beltrano', 'beltrano');
+
+
+-- Inserindo os dados das cartas
+
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Monkey D. Luffy', 'RARA', 'Monkey D Luffy.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Vismoke Sanji', 'RARA', 'Sanji.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Roronoa Zoro', 'RARA', 'Zoro.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Corazon', 'COMUM', 'Corazon.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Monkey D. Garp', 'RARA', 'Monkey D Garp.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Charlotte Katakuri', 'RARA', 'Charlotte Katakuri.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Edward Newgate', 'ÉPICA', 'Edward Newgate.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Sabo', 'COMUM', 'Sabo.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Shanks', 'ÉPICA', 'Shanks.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Gol D. Roger', 'ÉPICA', 'Gol D Roger.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Portgas D. Ace', 'COMUM', 'Portgas D Ace.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Silvers Rayleigh', 'RARA', 'Silvers Rayleigh.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Brook', 'COMUM', 'Brook.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Tony Tony Chopper', 'COMUM', 'Tony Tony Chopper.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Aokiji', 'RARA', 'Aokij.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Eustass Kid', 'COMUM', 'Eustass Kid.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Bartolomeo', 'COMUM', 'Bartolomeo.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Affe D. Drache', 'COMUM', 'Affe D. Drache.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Franky', 'COMUM', 'Franky.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Donquixote Doflamingo', 'RARA', 'Donquixote Doflamingo.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Boa Hancock', 'RARA', 'Boa Hancock.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Vinsmoke Reiju', 'COMUM', 'Vinsmoke Reiju.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Marco the Phoenix', 'COMUM', 'Marco the Phoenix.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Usopp', 'COMUM', 'Usopp.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Nico Robin', 'COMUM', 'Nico Robin.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Zeff', 'COMUM', 'Zeff.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Crocodile', 'COMUM', 'Crocodile.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Smoker', 'COMUM', 'Smoker.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Bentham', 'COMUM', 'Bentham.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Emporio Ivankov', 'COMUM', 'Emporio Ivankov.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Nekomamushi', 'COMUM', 'Nekomamushi.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Bartholomew Kuma', 'COMUM', 'Bartholomew Kuma.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Kizaru', 'RARA', 'Kizaru.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Rob Lucci', 'COMUM', 'Rob Lucci.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Juracule Mihawk', 'COMUM', 'Juracule Mihawk.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Cavendish', 'COMUM', 'Cavendish.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Perona', 'COMUM', 'Perona.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Benn Beckman', 'RARA', 'Benn Beckman.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Viola', 'COMUM', 'Viola.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Bepo', 'COMUM', 'Bepo.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Foxfire Kin\'emon', 'COMUM', 'Foxfire Kin\'emon.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Nefertari Vivi', 'COMUM', 'Nefertari Vivi.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Caesar Clown', 'COMUM', 'Caesar Clown.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Basil Hawkins', 'COMUM', 'Basil Hawkins.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Kaido', 'COMUM', 'Kaido.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Eneru', 'COMUM', 'Eneru.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Sengoku', 'COMUM', 'Sengoku.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Nami', 'COMUM', 'Nami.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Buggy', 'RARA', 'Buggy.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Carrot', 'COMUM', 'Carrot.png');
+INSERT INTO `onefigure`.`figure` (`name`, `rarity`, `path`) VALUES ('Monkey D Dragon', 'ESPECIAL', 'Dragon.png');
+
+
+-- Preenchendo o Álbum dos Jogadores
+
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('2', '1', '3');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('2', '2', '2');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('2', '3', '1');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('2', '4', '1');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('2', '5', '1');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('2', '17', '1');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('2', '16', '2');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('2', '13', '2');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('2', '20', '5');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('3', '11', '3');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('3', '7', '5');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('3', '10', '2');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('3', '13', '3');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('3', '3', '3');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('3', '15', '15');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('3', '12', '10');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('4', '4', '4');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('4', '1', '3');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('4', '2', '2');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('4', '3', '3');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('4', '19', '1');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('4', '18', '2');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('4', '17', '2');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('4', '13', '2');
+INSERT INTO `onefigure`.`album` (`idUser`, `idFigure`, `quantity`) VALUES ('4', '7', '2');
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,84 @@
-# CCF355-TP03
-Trabalho prático da disciplina de Sistemas Distribuídos e Paralelos da Universidade Federal de Visoça - campus Florestal (2021) com a temática de um álbum de figurinhas usando sockets.  s2
+# OneFigure
+
+Trabalho prático da disciplina de Sistemas Distribuídos e Paralelos da 🏫 Universidade Federal de Viçosa - Campus Florestal. 
+
+Este trabalho teve como objetivo desenvolver um sistema de figurinhas de nossa escolha, neste caso do anime One Piece, usando API de Soquetes para comunicar entre o Cliente e o Servidor usando XML como formato de mensagem. 
+
+## 🚀 Começando
+
+Essas instruções permitirão que você obtenha uma cópia do projeto em operação na sua máquina local para fins de desenvolvimento e teste.
+
+Consulte **Implantação** para saber como implantar o projeto.
+
+### 📋 Pré-requisitos
+
+De que coisas você precisa para instalar o software e como instalá-lo?
+
+```
+Dar exemplos
+```
+
+### 🔧 Instalação
+
+Uma série de exemplos passo-a-passo que informam o que você deve executar para ter um ambiente de desenvolvimento em execução.
+
+Diga como essa etapa será:
+
+```
+Dar exemplos
+```
+
+E repita:
+
+```
+Até finalizar
+```
+
+Termine com um exemplo de como obter dados do sistema ou como usá-los para uma pequena demonstração.
+
+## ⚙️ Executando os testes
+
+Explicar como executar os testes automatizados para este sistema.
+
+### 🔩 Analise os testes de ponta a ponta
+
+Explique que eles verificam esses testes e porquê.
+
+```
+Dar exemplos
+```
+
+### ⌨️ E testes de estilo de codificação
+
+Explique que eles verificam esses testes e porquê.
+
+```
+Dar exemplos
+```
+
+## 📦 Desenvolvimento
+
+Adicione notas adicionais sobre como implantar isso em um sistema ativo
+
+## 🛠️ Construído com
+
+Mencione as ferramentas que você usou para criar seu projeto
+
+* [Dropwizard](http://www.dropwizard.io/1.0.2/docs/) - O framework web usado
+* [Maven](https://maven.apache.org/) - Gerente de Dependência
+* [ROME](https://rometools.github.io/rome/) - Usada para gerar RSS
+
+## ✒️ Autores
+
+Mencione todos aqueles que ajudaram a levantar o projeto desde o seu início
+
+* **Um desenvolvedor** - *Trabalho Inicial* - [umdesenvolvedor](https://github.com/linkParaPerfil)
+* **Fulano De Tal** - *Documentação* - [fulanodetal](https://github.com/linkParaPerfil)
+
+Você também pode ver a lista de todos os [colaboradores](https://github.com/usuario/projeto/colaboradores) que participaram deste projeto.
+
+## 📄 Licença
+
+Este projeto está sob a licença (sua licença) - veja o arquivo [LICENSE.md](https://github.com/usuario/projeto/licenca) para detalhes.
+
+---
